04f3e4aab7bc3e4088e18787d9b7247067716f19
Brockton Point Media <aethus@brocktonpoint.com>
2015-11-16 15:49:54 -0800

    Adding MacOs Support

---
+++ CawBoxKit-Mac/CawBoxKit.h
@@ -0,0 +1,19 @@
+//
+//  CawBoxKit-Mac.h
+//  CawBoxKit-Mac
+//
+//  Created by Aethus Northcott on 2015-11-16.
+//  Copyright © 2015 CawBox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CawBoxKit-Mac.
+FOUNDATION_EXPORT double CawBoxKit_VersionNumber;
+
+//! Project version string for CawBoxKit-Mac.
+FOUNDATION_EXPORT const unsigned char CawBoxKit_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CawBoxKit_Mac/PublicHeader.h>
+
+
