d6913baf80983156504040f7799f1bdaa26370ff
Aethus Northcott <aethus@brocktonpoint.com>
2017-09-07 18:09:17 -0700

    Update indent

--- CawBoxKitTests/UIColorTests.swift
+++ CawBoxKitTests/UIColorTests.swift
@@ -1,54 +1,57 @@
 /*
-The MIT License (MIT)
+ The MIT License (MIT)
 
-Copyright (c) 2017 CawBox
+ Copyright (c) 2017 CawBox
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import UIKit
 import XCTest
 @testable import CawBoxKit
 
 class UIColorTests: XCTestCase {
-    func testHexRed () {
-        let red = UIColor.red ()
-        let redHex = UIColor (hex: "#FF0000")
-        
-        XCTAssert (red == redHex, "Failed to create Red from #FF0000")
-    }
-    func testHexGreen () {
-        let green = UIColor.green ()
-        let greenHex = UIColor (hex: "#00FF00")
-        
-        XCTAssert (green == greenHex, "Pass")
-    }
-    func testHexBlue () {
-        let blue = UIColor.blue ()
-        let blueHex = UIColor (hex: "#0000FF")
-        
-        XCTAssert (blue == blueHex, "Pass")
-    }
-    func testHexAlpha () {
-        let alpha = UIColor (red: 0, green: 0, blue: 0, alpha: 0.5)
-        let alphaHex = UIColor (hex: "#00000080")
-        
-        XCTAssert (alpha == alphaHex, "Pass")
-    }
+  func testHexRed() {
+    let red = UIColor.red
+    let redHex = UIColor(hex: "#FF0000")
+
+    XCTAssert(red == redHex, "Failed to create Red from #FF0000")
+  }
+
+  func testHexGreen() {
+    let green = UIColor.green
+    let greenHex = UIColor(hex: "#00FF00")
+
+    XCTAssert(green == greenHex, "Pass")
+  }
+
+  func testHexBlue() {
+    let blue = UIColor.blue
+    let blueHex = UIColor(hex: "#0000FF")
+
+    XCTAssert(blue == blueHex, "Pass")
+  }
+
+  func testHexAlpha() {
+    let alpha = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+    let alphaHex = UIColor(hex: "#00000080")
+
+    XCTAssert(alpha == alphaHex, "Pass")
+  }
 }
--- CawBoxKitTests/UserDataTests.swift
+++ CawBoxKitTests/UserDataTests.swift
@@ -1,63 +1,62 @@
 /*
-The MIT License (MIT)
+ The MIT License (MIT)
 
-Copyright (c) 2017 CawBox
+ Copyright (c) 2017 CawBox
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import XCTest
 @testable import CawBoxKit
 
 class UserDataTests: XCTestCase {
 
-    func testUserData () {
-        let stringKey = "string"
-        let stringValue = "Here is some test string."
-        
-        let intKey = "int"
-        let intValue = 12345
-        
-        let doubleKey = "double"
-        let doubleValue = 0.54321
-        
-        UserData<AnyObject>.clearAll ()
-        XCTAssert (UserData<String>.get (forKey: stringKey) == nil, "String value should be blank. (\(UserData<String>.get (forKey: stringKey)))")
-        XCTAssert (UserData<Int>.get (forKey: intKey) == nil, "Int value should be blank. (\(UserData<Int>.get (forKey: intKey)))")
-        XCTAssert (UserData<Double>.get (forKey: doubleKey) == nil, "Double value should be blank. (\(UserData<Double>.get (forKey: doubleKey)))")
-        
-        UserData<AnyObject>.setDefaults (defaults: [
-            stringKey: stringValue,
-            intKey: intValue,
-            doubleKey: doubleValue
-            ]
-        )
-        
-        XCTAssert (UserData<String>.get (forKey: stringKey) == stringValue, "String value incorrect.")
-        XCTAssert (UserData<Int>.get (forKey: intKey) == intValue, "Int value incorrect.")
-        XCTAssert (UserData<Double>.get (forKey: doubleKey) == doubleValue, "Double value incorrect.")
-        
-        UserData<AnyObject>.clear (keys: [stringKey])
-        XCTAssert (UserData<String>.get (forKey: stringKey) == nil, "String value should be blank.")
-        
-        UserData<AnyObject>.set (forKey: intKey, value: nil)
-        XCTAssert (UserData<Int>.get (forKey: intKey) == nil, "Int value should be blank.")
-    }
+  func testUserData() {
+    let stringKey = "string"
+    let stringValue = "Here is some test string."
 
+    let intKey = "int"
+    let intValue = 12345
+
+    let doubleKey = "double"
+    let doubleValue = 0.54321
+
+    UserData<AnyObject>.clearAll()
+    XCTAssert(UserData<String>.get(forKey: stringKey) == nil, "String value should be blank. (\(UserData<String>.get(forKey: stringKey)))")
+    XCTAssert(UserData<Int>.get(forKey: intKey) == nil, "Int value should be blank. (\(UserData<Int>.get(forKey: intKey)))")
+    XCTAssert(UserData<Double>.get(forKey: doubleKey) == nil, "Double value should be blank. (\(UserData<Double>.get(forKey: doubleKey)))")
+
+    UserData<AnyObject>.setDefaults(defaults: [
+      stringKey: stringValue,
+      intKey: intValue,
+      doubleKey: doubleValue,
+    ]
+    )
+
+    XCTAssert(UserData<String>.get(forKey: stringKey) == stringValue, "String value incorrect.")
+    XCTAssert(UserData<Int>.get(forKey: intKey) == intValue, "Int value incorrect.")
+    XCTAssert(UserData<Double>.get(forKey: doubleKey) == doubleValue, "Double value incorrect.")
+
+    UserData<AnyObject>.clear(keys: [stringKey])
+    XCTAssert(UserData<String>.get(forKey: stringKey) == nil, "String value should be blank.")
+
+    UserData<AnyObject>.set(forKey: intKey, value: nil)
+    XCTAssert(UserData<Int>.get(forKey: intKey) == nil, "Int value should be blank.")
+  }
 }
