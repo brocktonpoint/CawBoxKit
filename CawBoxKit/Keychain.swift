8bbb61b37615a85391a9ae6b01a324d9cc0ecb85
Aethus Northcott <aethus@brocktonpoint.com>
2017-09-07 18:10:31 -0700

    Format

--- CawBoxKit/Keychain.swift
+++ CawBoxKit/Keychain.swift
@@ -1,94 +1,101 @@
 /*
-The MIT License (MIT)
+ The MIT License (MIT)
 
-Copyright (c) 2017 CawBox
+ Copyright (c) 2017 CawBox
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import Foundation
 import Security
 
 public enum KeychainRequest {
-    case GenericPassword
-    case InternetPassword
-    case Certificate
-    
-    private var classType: CFString {
-        switch self {
-        case .GenericPassword:
-            return kSecClassGenericPassword
-        case .InternetPassword:
-            return kSecClassInternetPassword
-        case .Certificate:
-            return kSecClassCertificate
-        }
+  case GenericPassword
+  case InternetPassword
+  case Certificate
+
+  fileprivate var classTypeName: CFString {
+    switch self {
+    case .GenericPassword:
+      return kSecClassGenericPassword
+    case .InternetPassword:
+      return kSecClassInternetPassword
+    case .Certificate:
+      return kSecClassCertificate
     }
+  }
 }
+
 public struct KeychainTransaction {
-    public let request: KeychainRequest
-    public let service: String
-    public let attribute: String
-    
-    public var data: NSData? {
-        var results: Unmanaged<AnyObject>?
-        
-        let query: [NSString: AnyObject] = [
-            kSecClass: request.classType,
-            kSecAttrService: service,
-            kSecReturnData: kCFBooleanTrue,
-            kSecMatchLimitOne: kCFBooleanTrue
-        ]
-        
-        let status = withUnsafeMutablePointer(&results) { SecItemCopyMatching(query, UnsafeMutablePointer($0)) }
-        if status == noErr {
-            if let data = results?.takeUnretainedValue() as? NSData {
-                return data
-            }
-        }
-        
-        return nil
+  public let request: KeychainRequest
+  public let service: String
+  public let attribute: String
+
+  public var data: NSData? {
+    // var results: Unmanaged<AnyObject>?
+
+    let query: [NSString: AnyObject] = [
+      kSecClass: request.classTypeName,
+      kSecAttrService: service as NSString,
+      kSecReturnData: kCFBooleanTrue,
+      kSecMatchLimitOne: kCFBooleanTrue,
+    ]
+
+    // var results: UnsafeMutablePointer<CFTypeRef?>?
+    var output: AnyObject?
+    SecItemCopyMatching(query as NSDictionary, &output)
+    /*
+     let status = withUnsafeMutablePointer(to: &results) { SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0)) }
+     if status == noErr {
+     if let data = results?.takeUnretainedValue() as? NSData {
+     return data
+     }
+     }*/
+
+    return nil
+  }
+
+  public func set(value: NSData?) -> Bool {
+    // Clear the old value
+    _ = delete()
+
+    if let data = value {
+      let query: [NSString: AnyObject] = [
+        kSecClass: request.classTypeName,
+        kSecAttrService: service as NSString,
+        kSecAttrAccount: attribute as NSString,
+        kSecValueData: data,
+      ]
+
+      return SecItemAdd(query as CFDictionary, nil) == noErr
     }
-    public func set (value: NSData?) -> Bool {
-        // Clear the old value
-        delete ()
-        
-        if let data = value {
-            let query: [NSString: AnyObject] = [
-                kSecClass: request.classType,
-                kSecAttrService: service,
-                kSecAttrAccount: attribute,
-                kSecValueData: data
-            ]
-            
-            return SecItemAdd (query, nil) == noErr
-        }
-        
-        return false
-    }
-    public func delete () -> Bool {
-        let query: [NSString: AnyObject] = [
-            kSecClass: request.classType,
-            kSecAttrService: service,
-            kSecAttrAccount: attribute
-        ]
-        
-        return SecItemDelete (query) == noErr
-    }
+
+    return false
+  }
+
+  public func delete() -> Bool {
+    let query: [NSString: AnyObject] = [
+      kSecClass: request.classTypeName,
+      kSecAttrService: service as NSString,
+      kSecAttrAccount: attribute as NSString,
+    ]
+
+    return SecItemDelete(query as CFDictionary) == noErr
+  }
 }
--- CawBoxKit/UserData.swift
+++ CawBoxKit/UserData.swift
@@ -1,88 +1,94 @@
 /*
-The MIT License (MIT)
+ The MIT License (MIT)
 
-Copyright (c) 2017 CawBox
+ Copyright (c) 2017 CawBox
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import Foundation
 
-struct UserData <V> {
-    static func setDefaults (defaults: [String: AnyObject]) {
-        for (key, value) in defaults {
-            if UserData<AnyObject>.get (forKey: key) == nil {
-                UserData<AnyObject>.set (forKey: key, value: value)
-            }
-        }
+struct UserData<V> {
+  static func setDefaults(defaults: [String: AnyObject]) {
+    for (key, value) in defaults {
+      if UserData<AnyObject>.get(forKey: key) == nil {
+        UserData<AnyObject>.set(forKey: key, value: value)
+      }
     }
-    static func clear (keys: [String]) {
-        for key in keys {
-            set (forKey: key, value: nil)
-        }
+  }
+
+  static func clear(keys: [String]) {
+    for key in keys {
+      set(forKey: key, value: nil)
     }
-    static func clearAll () {
-        sync (values: [:])
+  }
+
+  static func clearAll() {
+    sync(values: [:])
+  }
+
+  static func get(forKey: String) -> V? {
+    return userData[forKey] as? V
+  }
+
+  static func set(forKey: String, value: V?) {
+    var localUserData = userData
+
+    if let object = value {
+      localUserData[forKey] = object as AnyObject
+    } else {
+      localUserData.removeValue(forKey: forKey)
     }
-    
-    static func get (forKey: String) -> V? {
-        return userData[forKey] as? V
-    }
-    static func set (forKey: String, value: V?) {
-        var localUserData = userData
-        
-        if let object = value.self {
-            localUserData[forKey] = object as? AnyObject
-        } else {
-            localUserData.removeValue (forKey: forKey)
-        }
-        
-        sync (values: localUserData)
-    }
+
+    sync(values: localUserData)
+  }
 }
 
 extension UserData {
-    private static var userDataURL: NSURL? {
-        let URL = try? NSFileManager.default().urlForDirectory (
-            .cachesDirectory,
-            in: .userDomainMask,
-            appropriateFor: nil,
-            create: true
-        )
-        
-        return URL?.appendingPathComponent ("userDataDefaults")
+  fileprivate static var userDataURL: URL? {
+    let userDataUrl = try? FileManager.default.url(
+      for: .cachesDirectory,
+      in: .userDomainMask,
+      appropriateFor: nil,
+      create: true
+    )
+
+    return userDataUrl?.appendingPathComponent("userDataDefaults")
+  }
+
+  fileprivate static var userData: [String: AnyObject] {
+    if let url = userDataURL, let data = try? Data(contentsOf: url) {
+      if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers),
+        let userData = json as? [String: AnyObject] {
+        return userData
+      }
     }
-    
-    private static var userData: [String: AnyObject] {
-        if let url = userDataURL, let data = NSData (contentsOf: url) {
-            if let json = try? NSJSONSerialization.jsonObject (with: data, options: .mutableContainers),
-                userData = json as? [String: AnyObject]{
-                    return userData
-            }
-        }
-        
-        return [:]
+
+    return [:]
+  }
+
+  fileprivate static func sync(values: [String: AnyObject]) {
+    if let url = userDataURL,
+      let data = try? JSONSerialization.data(
+        withJSONObject: values,
+        options: .prettyPrinted) {
+      try? data.write(to: url)
     }
-    
-    private static func sync (values: [String: AnyObject]) {
-        if let url = userDataURL, let data = try? NSJSONSerialization.data (withJSONObject: values, options: .prettyPrinted) {
-            data.write (to: url, atomically: true)
-        }
-    }
+  }
 }
